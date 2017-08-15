9c1234a7bac2fe4ddb88ea497ce88a70acf737cb
dbcx <jokr_c@163.com>
2017-08-15 14:39:41 +0800

    第一次初始化

---
+++ conn.php
@@ -0,0 +1,10 @@
+<?php 
+	//连接数据库
+	$con=mysqli_connect('localhost',"root",'root','msg') or die("数据库连接错误！");
+				   	mysqli_query($con,"set names utf8"); 
+	//自定义一个函数用来转换\n和空格 以便HTML能够读取
+    function toHtmlcode($content)  
+    {  
+        return $content = str_replace("\n","<br>",str_replace(" ", "&nbsp;", $content));  
+    }  
+ ?>
